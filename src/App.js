2a74726a01d4cd5a3d567d88ca5d116f4da11260
Issac Davis <issacndavis@gmail.com>
2023-01-11 15:41:00 -0600

    added my ip page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import HomepageLayout from './components/homepageLayout'
 import './App.css';
-
+import HomepageLayout from './components/HomepageLayout';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
+    <Router>
+    <Routes>
+      <Route path="/" element={<HomepageLayout />} />
+    </Routes>
+  </Router>
+
+
   return (
     <HomepageLayout />
   );
