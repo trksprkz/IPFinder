61278691fb9dc3c36fa4e661b774ee44896eedbb
Issac Davis <issacndavis@gmail.com>
2023-01-12 15:08:01 -0600

    refactored hide my ip

--- src/components/DontShowIp.jsx
+++ src/components/DontShowIp.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Form, Input, Button } from 'semantic-ui-react'
-
+import { useForm } from "react-hook-form";
 function DontShowIp() {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    }
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
     <Form.Group widths='equal'>
       <Form.Field
         id='form-input-control-first-name'
         control={Input}
         label='Enter an IP to hide'
         placeholder="IP Address"
+        {...register("ipAddress", { required: true })}
+
       />
     </Form.Group>
-    <Button>Submit</Button>
+    <Button type='submit'>Submit</Button>
   </Form>
   )
 }
